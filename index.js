e9068a40af552ec76940130f7cecde498cd53f1d
rhaynes3k <rhaynes3k@gmail.com>
2021-05-16 18:29:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let cards = [];
+function writeCards(peeps){
+  for(let c = 0; c < peeps.length; c++){
+    cards.push(`Thank you, ${peeps[c]}, for the wonderful surprise gift!`)
+  }
+  return cards;
+}
+function countDown(num){
+  let i=0;
+  while(i <= num){
+    console.log(num-i)
+    i++
+  }
+
+}
